55e5dd6b63599ecdd3e8e67409fa9d052bf235fc
Eirik Årdal <eirikaardal@gmail.com>
2023-07-31 16:16:19 +0200

    chore: cleanup and refactor

---
+++ src/lib/markdown.ts
@@ -0,0 +1,108 @@
+import 'highlight.js/styles/github-dark.css';
+import { marked } from 'marked';
+import { markedHighlight } from 'marked-highlight';
+import hljs from 'highlight.js';
+import { patchHtmlWithMkTextBlockDivs } from './patchHtml';
+
+// Enable syntax highlighting
+hljs.highlightAll();
+
+// To wrap HTML tags inside a new div, we use this flag to track wether we
+// have opened the HTML tag (we have inserted a <div> but not closed it yet with </div>).
+let isMkSectionDivOpen = false;
+
+const isH3Tag = (level: number) => level === 3;
+
+const addCustomCssClassesToMarkdown = {
+	heading(text: string, level: number) {
+		let html = '';
+
+		// To style each cheatsheet section correctly as cards, we need to detect
+		// where each section's content starts and ends.
+		//
+		// To track this, we're using <h3> tags. Whenever we see a <h3> tag, we start
+		// a new section by wrapping the following tags in a div with the class "mk-section": <div class="mk-section">{all section content}</div>
+		//
+		// This also means that when we see a <h3> tag we need to check if a mk-section div is already open and close it (end the previous section and start a new one).
+		//
+		// Example:
+		//
+		// This markdown:
+		//
+		// ### JavaScript cheats
+		//
+		// ```javascript
+		// const hello = "world";
+		// ```
+		//
+		// ### Go cheats
+		//
+		// ```go
+		// hello := "world"
+		// ```
+		//
+		// Should produce the following HTML:
+		//
+		// <div class="mk-section">
+		//   <h3>JavaScript cheats</h3>
+		//   <pre>...</pre>
+		// </div>
+		// <div class="mk-section">
+		//   <h3>Go cheats</h3>
+		//   <pre>...</pre>
+		// </div>
+
+		// If a div like <div class="mk-section"> is currently open, close it.
+		if (isH3Tag(level) && isMkSectionDivOpen) {
+			html += `</div>`;
+			isMkSectionDivOpen = false;
+		}
+
+		// If a div like <div class="mk-section"> is not open, start a new one.
+		if (isH3Tag(level) && !isMkSectionDivOpen) {
+			isMkSectionDivOpen = true;
+			html += `<div class="mk-section">`;
+		}
+
+		// Append the heading with the custom mk-h{level} class so we can target it with styling.
+		html += `<h${level} class="mk-h${level}">${text}</h${level}>`;
+		return html;
+	},
+
+	paragraph(text: string) {
+		// Set a class for <p> tags so we can target them with styling.
+		return `<p class="mk-p">${text}</p>`;
+	},
+	code(code: string, language: string) {
+		// Set a class for <code> tags so we can target them with styling.
+		return `<code class="mk-code language-${language}">${code}</code>`;
+	},
+	codespan(code: string) {
+		// TODO: Set a class for inline code markdown with `const foo = "bar"` syntax`
+		console.log('codespan:', code);
+		return code;
+	}
+};
+
+const hooks = {
+	postprocess(html: string) {
+		return patchHtmlWithMkTextBlockDivs(html);
+	}
+};
+
+marked.use(
+	{
+		mangle: false,
+		headerIds: false,
+		renderer: addCustomCssClassesToMarkdown,
+		hooks
+	},
+	markedHighlight({
+		highlight(code, lang) {
+			const language = hljs.getLanguage(lang) ? lang : 'plaintext';
+			return hljs.highlight(code, { language }).value;
+		}
+	})
+);
+
+export { marked };
---
+++ src/lib/patchHtml.ts
@@ -0,0 +1,121 @@
+// Walk backwards from each <pre> tag until we meet the section's heading (<h3>) or another <pre> tag
+// and wrap everything in between into a new div.
+function walkBackwards(preTags: NodeListOf<HTMLPreElement>, doc: Document) {
+	preTags.forEach((pre) => {
+		// Create a new div with class "mk-text-block"
+		const newDiv = doc.createElement('div');
+		newDiv.className = 'mk-text-block';
+
+		// Traverse backwards from the current pre tag
+		let prevSibling = pre.previousElementSibling;
+		while (
+			prevSibling &&
+			!(prevSibling.tagName.toLowerCase() === 'h3' || prevSibling.tagName.toLowerCase() === 'pre')
+		) {
+			const prevElement = prevSibling.previousElementSibling;
+			// Move the current previous sibling into the new div
+			newDiv.insertBefore(prevSibling, newDiv.firstChild);
+			prevSibling = prevElement;
+		}
+
+		// If we found any elements to move, insert the new div into the DOM.
+		if (newDiv.children.length > 0) {
+			// Insert the new div before the current pre tag
+			pre.parentNode?.insertBefore(newDiv, pre);
+		}
+	});
+}
+
+// Walk forwards from each <pre> tag until we meet another <pre> tag
+// and wrap everything in between into a new div.
+function walkForwards(preTags: NodeListOf<HTMLPreElement>, doc: Document) {
+	preTags.forEach((pre) => {
+		// Create a new div with class "mk-text-block"
+		const newDiv = doc.createElement('div');
+		newDiv.className = 'mk-text-block';
+
+		// Traverse forward from the current pre tag
+		let nextSibling = pre.nextElementSibling;
+		while (nextSibling && !(nextSibling.tagName.toLowerCase() === 'pre')) {
+			const nextElement = nextSibling.nextElementSibling;
+			// Move the current next sibling into the new div
+			newDiv.appendChild(nextSibling);
+			nextSibling = nextElement;
+		}
+
+		if (newDiv.children.length > 0) {
+			// Insert the new div after the current pre tag
+			pre.parentNode?.insertBefore(newDiv, pre.nextSibling);
+		}
+	});
+}
+
+/**
+ * To make each cheatsheet section as easy to read as possible, we want to wrap
+ * all content like <p> and <li> tags in divs that we can target with styling:
+ *
+ * <div class="mk-text-block">
+ *   ...all p tags
+ *   ...all li tags
+ *   ...etc
+ * </div>
+ * <pre><code></code></pre>
+ *
+ * This would be rather simple and could be done much like with the custom wrapper <div class="mk-section">
+ * as described in markdown.ts addCustomCssClassesToMarkdown.heading function's comments if only
+ * marked had a hook for manipulating <pre> tags like they do for "headings", "paragraphs", "code" and more (see markdown.ts).
+ *
+ * Unfortunately, they don't have such a hook for <pre> tags so we need to find these ourselves
+ * once the markdown has been processed to HTML and manipulate the final HTML ourselves to wrap
+ * the content we want in mk-text-block divs.
+ *
+ * patchHtmlWithMkTextBlockDivs ptakes care of this mess...
+ */
+export function patchHtmlWithMkTextBlockDivs(html: string) {
+	const parser = new DOMParser();
+	const doc = parser.parseFromString(html, 'text/html');
+
+	// The logic for wrapping content in mk-text-block divs is to find all <pre> tags
+	// and walk the sibling elements backwards until we meet a <h3> or <pre> tag.
+	// Every HTML element we found along the way should be added to a new div with the class mk-text-block.
+	//
+	// Example:
+	//
+	// This HTML:
+	//
+	// <h3>JavaScript functions</h3>
+	// <p>Some text</p>
+	// <p>More text</p>
+	// <ul>
+	//   <li>Remember this</li>
+	//   <li>And this</li>
+	// </ul>
+	// <pre>
+	//   <code>...code example</code>
+	// </pre>
+	//
+	// Should be converted to this:
+	//
+	// <h3>JavaScript functions</h3>
+	// <div class="mk-text-block">
+	//   <p>Some text</p>
+	//   <p>More text</p>
+	//   <ul>
+	//     <li>Remember this</li>
+	//     <li>And this</li>
+	//   </ul>
+	// </div>
+	// <pre>
+	//   <code>...code example</code>
+	// </pre>
+
+	// <pre> tags acts as stop-points. mk-text-block divs should end directly before <pre> tags or start directly after them.
+	// Find all the <pre> tags in the document:
+	const preTags = doc.querySelectorAll('pre');
+
+	walkBackwards(preTags, doc);
+	walkForwards(preTags, doc);
+
+	// Serialize the updated document back to an HTML string
+	return new XMLSerializer().serializeToString(doc);
+}
